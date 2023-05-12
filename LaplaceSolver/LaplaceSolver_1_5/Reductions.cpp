89ba108979a37fd425cc82547f55ea8b951a82ce
mal-cul1830 <munnikrishna@wisc.edu>
2023-05-11 20:41:28 -0500

    adding more cblas

--- LaplaceSolver/LaplaceSolver_1_5/Reductions.cpp
+++ LaplaceSolver/LaplaceSolver_1_5/Reductions.cpp
@@ -1,29 +1,47 @@
 #include "Reductions.h"
 
 #include <algorithm>
+#ifndef DO_NOT_USE_MKL
+#include <mkl.h>
+#endif 
 
 float Norm(const float (&x)[XDIM][YDIM][ZDIM])
 {
     float result = 0.;
-
+#ifdef DO_NOT_USE_MKL
 #pragma omp parallel for reduction(max:result)
     for (int i = 1; i < XDIM-1; i++)
     for (int j = 1; j < YDIM-1; j++)
     for (int k = 1; k < ZDIM-1; k++)
         result = std::max(result, std::abs(x[i][j][k]));
-
-    return result;
+#else
+    result = cblas_snrm2(
+        XDIM * YDIM * ZDIM,
+        &x[0][0][0],
+        1
+    );
+#endif
+    return (float) result;
 }
 
 float InnerProduct(const float (&x)[XDIM][YDIM][ZDIM], const float (&y)[XDIM][YDIM][ZDIM])
 {
     double result = 0.;
-
+#ifdef DO_NOT_USE_MKL
 #pragma omp parallel for reduction(+:result)
     for (int i = 1; i < XDIM-1; i++)
     for (int j = 1; j < YDIM-1; j++)
     for (int k = 1; k < ZDIM-1; k++)
         result += (double) x[i][j][k] * (double) y[i][j][k];
+#else
+    result = cblas_ddot(
+        XDIM * YDIM * ZDIM,
+        &x[0][0][0],
+        1,
+        &y[0][0][0],
+        1
+    );
 
+#endif
     return (float) result;
 }
